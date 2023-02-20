76e5abd90c5f23e3e1b60997e91ea1c6b75475f1
NOTGTM <107492638+NOTGTM@users.noreply.github.com>
2023-02-20 16:56:42 +0100

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,26 @@
+local UI = 
+
+local frame = script.Parent.Frame
+local tabs = {}
+local activeTab = nil
+
+tabs["Tab 1"] = UI.createTabFrame("Tab 1", frame, 1)
+tabs["Tab 2"] = UI.createTabFrame("Tab 2", frame, 2)
+
+local container = UI.createTabContainer(frame)
+
+local button1 = UI.createTabButton("Tab 1", frame, 1, function()
+    if activeTab ~= tabs["Tab 1"] then
+        activeTab = tabs["Tab 1"]
+        UI.showTabFrame(activeTab)
+    end
+end)
+
+local button2 = UI.createTabButton("Tab 2", frame, 2, function()
+    if activeTab ~= tabs["Tab 2"] then
+        activeTab = tabs["Tab 2"]
+        UI.showTabFrame(activeTab)
+    end
+end)
+
+UI.showTabFrame(tabs["Tab 1"])
