a23a26cbdc2c90a0119ee78afd844e0f04d06d46
NOTGTM <107492638+NOTGTM@users.noreply.github.com>
2023-02-20 17:04:24 +0100

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,53 +1,64 @@
-local UI = {}
+-- define UI library
+local Tabs = {}
+Tabs.__index = Tabs
 
-function UI.createTabButton(text, parent, layoutOrder, onClick)
-    local button = Instance.new("TextButton")
-    button.Text = text
-    button.BackgroundTransparency = 1
-    button.BorderSizePixel = 0
-    button.TextColor3 = Color3.new(1, 1, 1)
-    button.TextSize = 18
-    button.Size = UDim2.new(0, 80, 0, 30)
-    button.LayoutOrder = layoutOrder
-    button.Parent = parent
-    
-    button.MouseButton1Click:Connect(onClick)
-    
-    return button
+function Tabs.new()
+    local self = setmetatable({}, Tabs)
+    self.Tabs = {}
+    return self
 end
 
-function UI.createTabContainer(parent)
-    local container = Instance.new("Frame")
-    container.BackgroundTransparency = 1
-    container.BorderSizePixel = 0
-    container.Size = UDim2.new(1, 0, 1, -30)
-    container.Position = UDim2.new(0, 0, 0, 30)
-    container.Parent = parent
-    
-    return container
-end
-
-function UI.createTabFrame(name, parent, layoutOrder)
-    local frame = Instance.new("Frame")
-    frame.Name = name
-    frame.BackgroundTransparency = 1
-    frame.BorderSizePixel = 0
-    frame.Size = UDim2.new(1, 0, 1, 0)
+function Tabs:AddTab(name, frame)
+    self.Tabs[name] = frame
     frame.Visible = false
-    frame.LayoutOrder = layoutOrder
-    frame.Parent = parent
-    
-    return frame
 end
 
-function UI.showTabFrame(tabFrame)
-    for _, frame in pairs(tabFrame.Parent:GetChildren()) do
-        if frame:IsA("Frame") and frame.Name ~= tabFrame.Name then
-            frame.Visible = false
+function Tabs:SwitchToTab(name)
+    for tabName, tabFrame in pairs(self.Tabs) do
+        if tabName == name then
+            tabFrame.Visible = true
+        else
+            tabFrame.Visible = false
         end
     end
-    
-    tabFrame.Visible = true
 end
 
-return UI
+-- create tabs object and add tabs
+local tabs = Tabs.new()
+
+local tab1Frame = Instance.new("Frame")
+tab1Frame.Size = UDim2.new(1, 0, 1, 0)
+tab1Frame.BackgroundColor3 = Color3.new(1, 0, 0)
+
+local tab2Frame = Instance.new("Frame")
+tab2Frame.Size = UDim2.new(1, 0, 1, 0)
+tab2Frame.BackgroundColor3 = Color3.new(0, 1, 0)
+
+tabs:AddTab("Tab 1", tab1Frame)
+tabs:AddTab("Tab 2", tab2Frame)
+
+-- create UI for switching between tabs
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local tabsFrame = Instance.new("Frame")
+tabsFrame.Parent = screenGui
+tabsFrame.Size = UDim2.new(1, 0, 0, 50)
+tabsFrame.Position = UDim2.new(0, 0, 1, -50)
+tabsFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+
+for tabName, _ in pairs(tabs.Tabs) do
+    local button = Instance.new("TextButton")
+    button.Parent = tabsFrame
+    button.Size = UDim2.new(0, 100, 0, 50)
+    button.Position = UDim2.new(0, 100 * (#tabsFrame:GetChildren() - 1), 0, 0)
+    button.BackgroundColor3 = Color3.new(1, 1, 1)
+    button.Text = tabName
+    
+    button.MouseButton1Click:Connect(function()
+        tabs:SwitchToTab(tabName)
+    end)
+end
+
+-- switch to initial tab
+tabs:SwitchToTab("Tab 1")
